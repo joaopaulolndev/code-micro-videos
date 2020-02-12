a52dc8f5089fbbf61edb689eb95f4c2771998395
jlnascimento <joao.nascimento@pdmfc.com>
2020-02-12 13:39:28 +0000

    abstract controller

---
+++ app/Http/Controllers/Api/BasicCrudController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+abstract class BasicCrudController extends Controller
+{
+    protected abstract function model();
+
+    protected abstract function routeStore();
+
+    protected abstract function routeUpdate();
+
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+
+    public function index()
+    {
+        return Category::all();
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->rules);
+
+        $category = Category::create($request->all());
+        $category->refresh();
+        return $category;
+    }
+
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $this->validate($request, $this->rules);
+
+        $category->update($request->all());
+        return $category;
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->noContent();
+    }
+}
