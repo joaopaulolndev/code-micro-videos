c0af8eecf48c5e8d706c6cfe527f24cb988dfedd
jlnascimento <joao.nascimento@pdmfc.com>
2020-03-23 08:33:15 +0000

    phpstorm

--- backend/.phpstorm.meta.php
+++ backend/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-14 19:49:19.
+    * Generated on 2020-03-22 17:07:44.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
