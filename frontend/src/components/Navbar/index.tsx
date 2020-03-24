741c9afac7c8dfae57b59cdc99f8cc85558c5137
jlnascimento <joao.nascimento@pdmfc.com>
2020-03-24 09:29:34 +0000

    add Navbar component

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,11 +1,51 @@
 import * as React from 'react';
-import {AppBar, Toolbar} from "@material-ui/core";
+import {AppBar, Menu, MenuItem, Toolbar, IconButton, Typography, Button, makeStyles, Theme} from "@material-ui/core";
+import logo from '../../static/img/logo.png'
+import MenuIcon from "@material-ui/icons/Menu"
+
+const useStyles = makeStyles((theme:Theme) => ({
+    toolbar:{
+        backgroundColor: '#000000'
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center'
+    },
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')]:{
+            width: 170
+        }
+    }
+}));
 
 export const Navbar:React.FC = () => {
+    const classes = useStyles();
+
     return (
         <AppBar>
-            <Toolbar>
-                Texto
+            <Toolbar className={classes.toolbar}>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                >
+                    <MenuIcon />
+                </IconButton>
+
+                <Menu
+                    id="menu-appbar"
+                    open={false}
+                >
+                    <MenuItem>Categorias</MenuItem>
+                </Menu>
+
+                <Typography className={classes.title}>
+                    <img src={logo} alt="Codeflix" className={classes.logo}/>
+                </Typography>
+                <Button color="inherit">Login</Button>
             </Toolbar>
         </AppBar>
     );
