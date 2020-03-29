2315afa50dd5d8e9de2808c919a3eb10f3381b69
jlnascimento <joao.nascimento@pdmfc.com>
2020-03-29 19:18:56 +0100

    categories list

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,13 +1,38 @@
-// @flow 
 import * as React from 'react';
+import MUIDataTable, {MUIDataTableColumn} from "mui-datatables";
 
+const columnsDefinition:MUIDataTableColumn[] = [
+    {
+        name: "name",
+        label: "Nome"
+    },
+    {
+        name: "is_active",
+        label: "Ativo?"
+    },
+    {
+        name: "created_at",
+        label: "Criado em"
+    },
+];
+
+const data = [
+    {name:"teste1", is_active: true, created_at: "2019-12-12"},
+    {name:"teste2", is_active: false, created_at: "2019-12-13"},
+    {name:"teste3", is_active: true, created_at: "2019-12-14"},
+    {name:"teste4", is_active: false, created_at: "2019-12-15"}
+];
 type Props = {
     
 };
 const Table = (props: Props) => {
     return (
         <div>
-            tabela
+            <MUIDataTable
+                title="Listagem de Categorias"
+                columns={columnsDefinition}
+                data={data}
+            />
         </div>
     );
 };
