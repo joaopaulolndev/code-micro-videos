5ff833453833f49365ff03f5c8254cdc1b817578
jlnascimento <joao.nascimento@pdmfc.com>
2020-04-06 08:53:20 +0100

    fix error form category

--- backend/.phpstorm.meta.php
+++ backend/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-04-04 10:26:56.
+    * Generated on 2020-04-05 15:26:31.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- backend/_ide_helper.php
+++ backend/_ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.12.0 on 2020-04-04 10:26:51.
+ * Generated for Laravel 6.12.0 on 2020-04-05 15:26:27.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
